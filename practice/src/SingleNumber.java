b0345758f67ce00fa3ec29b5cf29d289853d6f44
Jinyou Shi <yutuoygy@gmail.com>
2017-12-14 21:12:07 -0800

    LC-136 SingleNumber - Add main method

--- practice/src/SingleNumber.java
+++ practice/src/SingleNumber.java
@@ -1,6 +1,17 @@
 public class SingleNumber {
 
-    public int singleNumber(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{};
+        int[] nums2 = new int[]{1, 1, 2, 3, 3, 4, 4, 5, 5};
+        int[] nums3 = new int[]{1, 1, 2, 3, 3, 4, 4};
+        int[] nums4 = new int[]{1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+        System.out.println(singleNumber(nums1));
+        System.out.println(singleNumber(nums2));
+        System.out.println(singleNumber(nums3));
+        System.out.println(singleNumber(nums4));
+    }
+
+    public static int singleNumber(int[] nums) {
         int res = 0;
         for (int i = 0; i < nums.length; i++) {
             res = res^nums[i];
