0eba81aaa91d6a657d7f8cade65262b4bc3c2aef
Jinyou Shi <yutuoygy@gmail.com>
2017-12-15 16:26:15 -0800

    Practice 3: solve LongestPalindromicSubstring without repeat number.

---
+++ practice/src/jinyou/ReverseLinkedList.java
@@ -0,0 +1,25 @@
+package jinyou;
+
+public class ReverseLinkedList {
+    /*
+     * reverse a LinkedList
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x){val = x;}
+    }
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+}
