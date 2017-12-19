880112b7fdbf2e53524ef8aba584ffe9987415cb
Jinyou Shi <yutuoygy@gmail.com>
2017-12-19 15:02:56 -0800

    Practice 11: add explanation and comments

--- practice/src/jinyou/ContainerWithMostWater.java
+++ practice/src/jinyou/ContainerWithMostWater.java
@@ -22,12 +22,25 @@ public class ContainerWithMostWater {
     public int maxArea(int[] height) {
         int max = 0;
         int left = 0, right = height.length - 1;
+        /*
+        max used to record the most water can be contained in
+        the area, the left bound start with 0, and the right bound
+        start from the right bound.
+         */
         while (left < right) {
             max = Math.max(Math.min(height[left], height[right]) * (right - left), max);
             if (height[left] < height[right]) {
                 left++;
             } else {
                 right--;
+                /*
+                we use left++ or right-- to iterate the whole array,
+                the reason why we choose the shorter part is beacuse
+                it is a water container.
+
+                then we always move the shorter height, then we will
+                finally find the result.
+                 */
             }
         }
         return max;
