0c5df18f7bff533373e42316444a5a3ffab520a9
Jinyou Shi <yutuoygy@gmail.com>
2017-12-19 18:33:18 -0800

    Practice 13: add comment on it

--- practice/src/jinyou/RomanToInteger.java
+++ practice/src/jinyou/RomanToInteger.java
@@ -22,11 +22,25 @@ public class RomanToInteger {
         map.put('D', 500);
         map.put('M', 1000);
         int len = s.length(), result = map.get(s.charAt(len - 1));
+        /*
+        as the last digit can be a integer determined by the last digit of roman for example
+        the Roma digit MDLV, the last digit can always be 5.
+         */
         for (int i = len - 2; i >= 0; i--) {
+            /*
+            then we start from the digit before the last digit as s.length() - 2, and iterate from the
+            small digit to larger digit, which means from right to left.
+             */
             if (map.get(s.charAt(i)) >= map.get(s.charAt(i + 1))) {
                 result += map.get(s.charAt(i));
             } else {
                 result -= map.get(s.charAt(i));
+                /*
+                we do the calculation if s.CharAt(i) < s.CharAt(i+1) we should
+                deduct it, as IV - 4 = 5 -1.
+                or when s.CharAt(i) > s.CharAt(i+1) we should add it, as VI - 6
+                 = 5 + 1;
+                 */
             }
 
         }
