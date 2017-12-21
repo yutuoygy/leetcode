690d601837b579937f4856e2601cf2479abc6c12
Jinyou Shi <yutuoygy@gmail.com>
2017-12-20 16:29:14 -0800

    Practice 15: 3Sum with comments

--- practice/src/jinyou/ThreeSum.java
+++ practice/src/jinyou/ThreeSum.java
@@ -23,8 +23,16 @@ public class ThreeSum {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i + 2 < nums.length; i++) {
+            /*
+            i + 2 < nums.length as we have two pointer k and j behind i
+            and index of k, j should never equals to index i, so that's
+            why we make i < nums.length - 2.
+             */
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
+                /*
+                skip the same result
+                 */
             }
             int j = i + 1, k = nums.length - 1;
             int target =- nums[i];
@@ -35,14 +43,24 @@ public class ThreeSum {
                     k--;
                     while (j < k && nums[j] == nums[j - 1]) {
                         j++;
+                        /*
+                        skip the same result as nums[j] = nums[j - 1]
+                         */
                     }
                     while (j <k && nums[k] == nums[k + 1]) {
                         k--;
+                        /*
+                        skip the same result as nums[k] = nums[k + 1]
+                         */
                     }
                 } else if (nums[j] + nums[k] > target) {
                     k--;
                 } else {
                     j++;
+
+                    /*
+                    as the array has been sorted, so the number is in a increase way.
+                     */
                 }
             }
         }
