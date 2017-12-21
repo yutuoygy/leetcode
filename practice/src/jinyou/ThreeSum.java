b43ad60aa59474cfff7f43c5e08b32e234b5ac94
Jinyou Shi <yutuoygy@gmail.com>
2017-12-20 16:09:44 -0800

    Practice 15: 3Sum

---
+++ practice/src/jinyou/ThreeSum.java
@@ -0,0 +1,53 @@
+package jinyou;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    /*
+    Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+    Find all unique triplets in the array which gives the sum of zero.
+    Note: The solution set must not contain duplicate triplets.
+
+    For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+    A solution set is:
+    [
+    [-1, 0, 1],
+    [-1, -1, 2]
+    ]
+     */
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1, k = nums.length - 1;
+            int target =- nums[i];
+            while (j < k) {
+                if (nums[j] + nums[k] == target) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    while (j <k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                } else if (nums[j] + nums[k] > target) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return result;
+
+    }
+
+}
