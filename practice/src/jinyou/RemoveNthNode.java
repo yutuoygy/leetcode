7e44c866deb9c1acb1788eb5f56b10c74dcab04f
Jinyou Shi <yutuoygy@gmail.com>
2017-12-20 22:00:28 -0800

    Practice 19: Remove Nth Node from End

---
+++ practice/src/jinyou/RemoveNthNode.java
@@ -0,0 +1,37 @@
+package jinyou;
+
+import java.util.List;
+
+public class RemoveNthNode {
+    /*
+    Given a linked list, remove the nth node from the end of list and return its head.
+
+    For example
+    Given linked list: 1->2->3->4->5, and n = 2.
+
+    After removing the second node from the end, the linked list becomes 1->2->3->5.
+
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {val = x;}
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n){
+        ListNode start = new ListNode(0);
+        ListNode slow =start, fast = start;
+        slow.next = head;
+
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return start.next;
+    }
+
+}
